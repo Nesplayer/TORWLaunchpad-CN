da4a9e563804d78480d77d858ea6ecc4ee451029
AndriesWorks <andries46565@gmail.com>
2025-04-27 15:42:09 +0200

    added translation stuffs

---
+++ LaunchpadReloaded/Translations/TranslationPool.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace LaunchpadReloaded.Translations
+{
+   public struct TranslationPool
+   {
+     //Expandable 
+     public string English;
+     public string Spanish;
+     public string French;
+      
+     public TranslationPool(string english, string spanish = null, string french = null)
+     {
+       English = english;
+       Spanish = spanish;
+       French = french;
+     }
+   }
+}
